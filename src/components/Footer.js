3b7c623a103405a174869a20e49619e0c0d227ab
RemWinter <97475844+RemWinter@users.noreply.github.com>
2022-07-12 20:38:00 +0100

    fixed images not loading

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,19 +5,19 @@ const footer = () => {
     <footer className='container'>
       <div className="footer-left">
         <div className="footer-left-content">
-        <img className='logo' src="./design-dev-test/logo.svg" alt="" />
+        <img className='logo' src="https://raw.githubusercontent.com/RemWinter/design-dev-test/main/public/logo.svg" alt="" />
         <div className="social-icons">
-          <img src="./design-dev-test/twitter.svg" alt="" className="icon" />          
-          <img src="./design-dev-test/linkedin.svg" alt="" className="icon" />        
-          <img src="./design-dev-test/google.svg" alt="" className="icon" />          
-          <img src="./design-dev-test/facebook.svg" alt="" className="icon" />          
+          <img src="https://raw.githubusercontent.com/RemWinter/design-dev-test/main/public/twitter.svg" alt="" className="icon" />          
+          <img src="https://raw.githubusercontent.com/RemWinter/design-dev-test/main/public/linkedin.svg" alt="" className="icon" />        
+          <img src="https://raw.githubusercontent.com/RemWinter/design-dev-test/main/public/google.svg" alt="" className="icon" />          
+          <img src="https://raw.githubusercontent.com/RemWinter/design-dev-test/main/public/facebook.svg" alt="" className="icon" />          
         </div>
         </div>
       </div>
 
       <div className="footer-right">
         <div className="img-div">
-          <img src="./design-dev-test/twitter_embed.png" alt="" />
+          <img src="https://raw.githubusercontent.com/RemWinter/design-dev-test/main/public/twitter_embed.png" alt="" />
         </div>
       </div>
       
